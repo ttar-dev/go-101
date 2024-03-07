7be549341f32d291130980aeaf5b9ca54f215bd8
rvp-nattawut <ta.nattawut@icloud.com>
2024-03-07 10:58:22 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "go-101/package1"
+
+func main() {
+	package1.SayHello("Gopher")
+}
---
+++ package1/package1.go
@@ -0,0 +1,11 @@
+package package1
+
+import "fmt"
+
+func HelloWorld()  {
+	fmt.Println("Hello, World!")
+}
+
+func SayHello(name string) {
+	fmt.Println("Hello,", name)
+}
